240e2f0a4528a36e39f416b83f60ced22f265f9a
Renan Rodrigues <r.rodrigues@avivatec.com.br>
2025-03-10 18:47:47 -0300

    Add project files.

---
+++ Colections/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursoCSharp.Colections
+{
+
+    /// <summary>
+    /// Estrutura chave valor Exemplo       
+    /// </summary>
+    public static class Dictionary
+    {
+        //Declaraçao
+        public static Dictionary<int, string> itens = new Dictionary<int, string>()
+        {
+            [1] = "a",
+            [2] = "b",
+            [3] = "c",
+        };
+        
+        public static void Executa()
+        {
+            //Adicionar 
+            itens.Add(10, "d");
+
+            //Como percorrer
+            foreach (KeyValuePair<int,string> pair in itens)
+            {
+               Console.WriteLine($"CHAVE: { pair.Key},VALOR:{pair.Value}");
+            }
+        }
+        public static void ObterValorPorIdChave(int chave)
+        {
+            if(itens.TryGetValue(chave,out string valor))
+            {                
+                   Console.WriteLine($"O valor encontrado é : {valor}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Nenhum valor encontrado para a Chave : {chave}");
+            }
+
+        }
+
+        
+
+
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecursoCSharp.Colections;
+
+
+
+#region Dicionario
+Dictionary.Executa();
+Dictionary.ObterValorPorIdChave(1);
+#endregion
