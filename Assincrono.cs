037c2610e61dcd287021ac42b594af97fafc7b0a
Renan Rodrigues <r.rodrigues@avivatec.com.br>
2025-03-10 19:28:31 -0300

    Assincrono

---
+++ Assincrono.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursoCSharp
+{
+
+    /// <summary>
+    /// Permite que a execução de uma operação seja executada separadamente da theat principal até que a operação seja concluida.
+    /// 
+    /// </summary>
+    public static class Assincrono
+    {
+    
+        public static async Task Executa(int tempoDeEspera)
+        {
+            await Task.Delay(tempoDeEspera);
+        }
+    
+    
+    
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ using RecursoCSharp.Colections;
 #endregion
 
 #region Tupla
-var teste = Tupla.ValidaNomeTamanhoNomeUsuario("Teste");
-Console.WriteLine($"{teste.valido} - { teste.Nome}");
+//var teste = Tupla.ValidaNomeTamanhoNomeUsuario("Teste");
+//Console.WriteLine($"{teste.valido} - { teste.Nome}");
+
+#endregion
+
+#region Asincrono
+int tempoEspera = 5000;
+Console.WriteLine($"Iniciou processamento.");
+await Assincrono.Executa(tempoEspera);
+Console.WriteLine($"Aguardou {(tempoEspera/1000)} segundos....");
+
+
+
 
 #endregion
